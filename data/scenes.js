e8962a368505b33c0c22aebb55ae44d512a89127
Baibhab-7279 <mrbaibhab7279@gmail.com>
2026-06-09 01:03:28 +0530

    v5_1 first commit

---
+++ data/scenes.js
@@ -0,0 +1,17 @@
+window.PANORAMA_SCENES = [
+    {
+        "id": "cafe",
+        "title": "Cafe",
+        "file": "cafe.png"
+    },
+    {
+        "id": "lobby",
+        "title": "Lobby",
+        "file": "lobby.png"
+    },
+    {
+        "id": "reception",
+        "title": "Reception",
+        "file": "reception.png"
+    }
+];
